d436dda34160067a5498e1231f60271ea766ee37
Strax´s Development <107302328+Straxeu@users.noreply.github.com>
2023-01-17 18:24:31 +0100

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,14 @@
+fx_version 'cerulean'
+games { 'gta5' }
+
+author 'Brezel Services'
+description 'Loadingscreen V4 By Patbre#0103'
+version '1.0'
+lua54 'yes'
+
+
+loadscreen 'html/index.html'
+
+files {
+    "html/*"
+}
